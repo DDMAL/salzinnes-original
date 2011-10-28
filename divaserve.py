d88c4d7de164e7cb13b474473c06b3384fefa85c
Alastair Porter <alastair.porter@mail.mcgill.ca>
2011-10-28 18:34:54 -0400

    Process images during startup, not on first GET
    Requires tilesize to be given in the constructor. This might need to
    change if clients are to send custom tile sizes.

--- divaserve.py
+++ divaserve.py
@@ -1,74 +1,69 @@
 import os
-import glob
+import sys
 import math
 from vipsCC import VImage
+from operator import itemgetter
 
 class DivaServe(object):
-    def __init__(self, directory, **kwargs):
+    def __init__(self, directory, t=256, mode="memory"):
         """ <directory> is the directory of images to serve 
             "mode" can be either "memory" or "disk" for the cache mode to use
             if "mode" is "disk," you must also set "cachedir".
-
+            <t> is be the tilesize. Default 256
         """
+
         self.imgdir = directory
         self.images = {}
         self.data = {}
+        self.lowest_max_zoom = 0
 
-    def get(self, zoom, t=None):
-        """ <t> should be the tilesize. Default 256 """
-        if t:
-            til_wid = t
-            til_hei = t
-        else:
-            til_wid = 256
-            til_hei = 256
-        
-        lowest_max_zoom = 0;
-        pgs = []
+        self.til_wid = t
+        self.til_hei = t
 
+        lmz = 0
+        # Read image sizes on startup
+        print >> sys.stderr, "loading images..."
+        for i,f in enumerate(os.listdir(self.imgdir)):
+            if os.path.splitext(f)[1] not in (".tif", ".tiff"):
+                continue
+            img = VImage.VImage(os.path.join(self.imgdir, f))
+            img_wid = img.Xsize()
+            img_hei = img.Ysize()
+            del img
+
+            max_zoom = self._get_max_zoom_level(img_wid, img_hei, self.til_wid, self.til_hei)
+            if max_zoom > lmz:
+                lmz = max_zoom
+ 
+            self.images[i] = {
+                'mx_w': img_wid,
+                'mx_h': img_hei,
+                'mx_z': max_zoom,
+                'fn': os.path.join(self.imgdir, f)
+            }
+
+        self.lowest_max_zoom = lmz
+        print >> sys.stderr, "images loaded"
+
+
+    def get(self, zoom):
         if zoom in self.data.keys() and len(self.data[zoom]) > 0:
             return self.data[zoom]
 
-        if not self.images.keys():
-            i = 0
-
-            for f in glob.glob(os.path.join(self.imgdir, "*.tif")):
-                img = VImage.VImage(f)
-                img_wid = img.Xsize()
-                img_hei = img.Ysize()
-                del img
-
-                max_zoom = self._get_max_zoom_level(img_wid, img_hei, til_wid, til_hei)
-                if max_zoom > lowest_max_zoom:
-                    lowest_max_zoom = max_zoom
-                
-                self.images[i] = {
-                    'mx_w': img_wid,
-                    'mx_h': img_hei,
-                    'mx_z': max_zoom,
-                    'fn': f
-                }
-
-                self.images['lmx'] = lowest_max_zoom
-                i += 1
-        else:
-            lowest_max_zoom = self.images['lmx']
-
-        if zoom > lowest_max_zoom:
+        if zoom > self.lowest_max_zoom:
             zoom = 0
         elif zoom < 0:
             zoom = 0
 
         mx_h = mx_w = t_wid = t_hei = num_pages = max_ratio = 0
+        pgs = []
 
-        for k,v in self.images.iteritems():
-            if k == 'lmx':
-                continue
-            h = self._incorporate_zoom(v['mx_h'], lowest_max_zoom - zoom)
-            w = self._incorporate_zoom(v['mx_w'], lowest_max_zoom - zoom)
+        for v in self.images.itervalues():
+            h = self._incorporate_zoom(v['mx_h'], self.lowest_max_zoom - zoom)
+            w = self._incorporate_zoom(v['mx_w'], self.lowest_max_zoom - zoom)
 
-            c = math.ceil(w / float(til_wid))
-            r = math.ceil(h / float(til_hei))
+            c = math.ceil(w / float(self.til_wid))
+            r = math.ceil(h / float(self.til_hei))
             m_z = v['mx_z']
             fn = v['fn']
             
@@ -92,6 +87,7 @@ class DivaServe(object):
             t_wid += w
             t_hei += h
             num_pages += 1
+        pgs.sort(key=itemgetter("fn"))
 
         if num_pages > 0:
             a_wid = t_wid / float(num_pages)
@@ -112,7 +108,7 @@ class DivaServe(object):
             self.data[zoom] = {
                 'item_title': title,
                 'dims': dims,
-                'max_zoom': lowest_max_zoom,
+                'max_zoom': self.lowest_max_zoom,
                 'pgs': pgs
             }
         else:
@@ -121,7 +117,7 @@ class DivaServe(object):
         return self.data[zoom]
 
     def _get_max_zoom_level(self, iwid, ihei, twid, thei):
-        largest_dim = iwid if iwid > ihei else ihei
+        largest_dim = max(iwid, ihei)
         t_dim = twid if iwid > ihei else thei
 
         zoom_levels = math.ceil(math.log((largest_dim + 1) / float(t_dim) + 1, 2))
